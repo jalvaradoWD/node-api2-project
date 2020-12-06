9890a3efbb4d6746fbe4c2497218f5115e1a94e5
jalvaradoWD <jalvarado1337@lavabit.com>
2020-12-05 20:42:07 -0600

    Removed the line of code that tested out if the express server is running correctly.

--- index.js
+++ index.js
@@ -9,8 +9,4 @@ server.use(express.json());
 // Endpoints
 server.use("/api/posts", postRoutes);
 
-server.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
-});
-
 server.listen(8080, () => console.log(`Server is on http://localhost:8080/`));
